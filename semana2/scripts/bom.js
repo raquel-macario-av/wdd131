1db0b6b3c3277f38750cd65886f7dddafa231df5
avwrrdigital-code <av.wrrdigital@gmail.com>
2026-09-23 08:16:29 -0300

    commit

---
+++ semana2/scripts/bom.js
@@ -0,0 +1,24 @@
+const input = document.querySelector('#capfav');
+const botão = document.querySelector('button');
+const lista = document.querySelector('______');
+
+const li = document.createElement('li)');
+const botaoExcluir = document.createElement('button');
+li.textContent = input.value;
+botaoExcluir.textContent = '❌'
+li.append(botaoexcluir);
+lista.append(li);
+button.addEventListener('click', function () {
+    
+});
+if (input.value.trim() !== '') { ... }
+botaoExcluir.addEventListener('click', function () {
+    
+});
+input.value = '';
+input.focus();
+
+
+
+
+
